973976c4b623cd9fbee44814c9d4d489235ae621
Venelin Velev <vmvelev@gmail.com>
2023-11-05 08:25:28 +0200

    layout

--- app/page.js
+++ app/page.js
@@ -1,113 +1,233 @@
-import Image from 'next/image'
+// import Axios from 'axios';
+// import { setupCache } from 'axios-cache-interceptor';
+
+// const axios = setupCache(Axios);
+
+// async function getData() {
+//   const fluxCurreny = axios.get('https://explorer.runonflux.io/api/currency')
+//   const nodeCount = axios.get('https://api.runonflux.io/daemon/getzelnodecount')
+//   const benchVersion = axios.get('https://fluxnode.app.runonflux.io/api/v1/bench-version')
+//   const daemonInfo = axios.get('https://api.runonflux.io/daemon/getinfo')
+//   const richestAddressList = axios.get('https://explorer.runonflux.io/api/statistics/richest-addresses-list')
+//   const fluxStats = axios.get('https://stats.runonflux.io/fluxinfo?projection=apps.runningapps.Image')
+//   const deterministicNodes = axios.get('https://api.runonflux.io/daemon/viewdeterministiczelnodelist')
+//   const wordpressCount = axios.get('https://jetpackbridge.runonflux.io/api/v1/wordpress.php?action=COUNT')
+//   const resources = axios.get('https://stats.runonflux.io/fluxinfo?projection=apps.resources')
+
+//   const [
+//     fluxCurrenyResponse,
+//     nodeCountResponse,
+//     benchVersionResponse,
+//     daemonInfoResponse,
+//     richestAddressListResponse,
+//     fluxStatsResponse,
+//     deterministicNodesResponse,
+//     wordpressCountResponse,
+//     resourcesResponse
+//   ] = await Promise.all(
+//     [
+//       fluxCurreny,
+//       nodeCount,
+//       benchVersion,
+//       daemonInfo,
+//       richestAddressList,
+//       fluxStats,
+//       deterministicNodes,
+//       wordpressCount,
+//       resources
+//     ]
+//   );
+
+//   return {
+//     fluxCurreny: fluxCurrenyResponse.data,
+//     nodeCount: nodeCountResponse.data,
+//     benchVersion: benchVersionResponse.data,
+//     daemonInfo: daemonInfoResponse.data,
+//     richestAddressList: richestAddressListResponse.data,
+//     fluxStats: fluxStatsResponse.data,
+//     deterministicNodes: deterministicNodesResponse.data,
+//     wordpressCount: wordpressCountResponse.data,
+//     resources: resourcesResponse.data
+//   };
+// }
+
+// function getUniquePaymentAddresses(nodes) {
+//   const uniquePaymentAddresses = new Set();
+//   nodes.forEach((item) => {
+//     uniquePaymentAddresses.add(item.payment_address);
+//   });
+//   const uniqueAddresses = Array.from(uniquePaymentAddresses).length;
+//   return uniqueAddresses;
+// }
+
+// function getRunningApps(stats) {
+//   let totalRunning = 0;
+//   let streamrCount = 0;
+//   let presearchCount = 0;
+//   let watchtowerCount = 0;
+
+//   for (const stat of stats) {
+//     totalRunning += stat.apps.runningapps.length;
+//     for (const image of stat.apps.runningapps) {
+//       if (image.Image === "presearch/node:latest") {
+//         presearchCount++;
+//       }
+//       if (image.Image === "streamr/broker-node:latest") {
+//         streamrCount++;
+//       }
+//       if (image.Image === "containrrr/watchtower:latest") {
+//         watchtowerCount++;
+//       }
+//     }
+//   }
+//   const finalTotalRunning = totalRunning - watchtowerCount;
+//   return {
+//     presearch: presearchCount,
+//     streamr: streamrCount,
+//     total: finalTotalRunning,
+//   }
+// }
+
+// function getNodeCount(nodes) {
+//   const cumulusCount = nodes.data["cumulus-enabled"];
+//   const nimbusCount = nodes.data["nimbus-enabled"];
+//   const stratusCount = nodes.data["stratus-enabled"];
+//   return {
+//     cumulus: cumulusCount,
+//     nimbus: nimbusCount,
+//     stratus: stratusCount,
+//   }
+// }
+
+// function getEmptyNodes(nodes) {
+//   const emptyNodes = nodes.filter((data) => data.apps.resources.appsRamLocked === 0).length;
+//   return emptyNodes;
+// }
+
+// function getUtilizedResources(nodes) {
+//   const utilizedCPU = nodes.reduce((acc, data) => acc + data.apps.resources.appsCpusLocked, 0)
+//   const utilizedRAM = nodes.reduce((acc, data) => acc + data.apps.resources.appsRamLocked, 0) / 1000000;
+//   const utilizedStorage = nodes.reduce((acc, data) => acc + data.apps.resources.appsHddLocked, 0) / 1000;
+//   return {
+//     utilizedCPU,
+//     utilizedRAM,
+//     utilizedStorage,
+//   }
+// }
+
+// export default async function Test() {
+//   const data = await getData();
+//   const uniqueAddresses = getUniquePaymentAddresses(data.deterministicNodes.data);
+//   const runningApps = getRunningApps(data.fluxStats.data);
+//   const wordpressCount = data.wordpressCount;
+//   const nodesCount = getNodeCount(data.nodeCount);
+//   const allNodesCount = (nodesCount.cumulus + nodesCount.nimbus + nodesCount.stratus);
+//   const emptyNodes = getEmptyNodes(data.resources.data);
+//   const utilizedNodes = allNodesCount - emptyNodes;
+//   const utilizedNodesPercentage = (utilizedNodes / allNodesCount) * 100;
+//   const utilizedResources = getUtilizedResources(data.resources.data);
+//   const currentHeight = data.daemonInfo.data.blocks
+//   return (
+//     <div>
+//       <h1>Unique addresses - {uniqueAddresses}</h1>
+//       <h1>Presearch nodes - {runningApps.presearch}</h1>
+//       <h1>Streamr nodes - {runningApps.streamr}</h1>
+//       <h1>Total running - {runningApps.total}</h1>
+//       <h1>Wordpress - {wordpressCount}</h1>
+//       <h1>Cumulus nodes - {nodesCount.cumulus}</h1>
+//       <h1>Nimbus nodes - {nodesCount.nimbus}</h1>
+//       <h1>Stratus nodes - {nodesCount.stratus}</h1>
+//       <h1>Utilized nodes {utilizedNodes}</h1>
+//       <h1>Percentage utilized nodes - {utilizedNodesPercentage}</h1>
+//       <h1>Utilized CPUs - {utilizedResources.utilizedCPU}</h1>
+//       <h1>Utilizied RAM - {utilizedResources.utilizedRAM} TB</h1>
+//       <h1>Utilized Storage - {utilizedResources.utilizedStorage} TB</h1>
+//       <h1>Current block height - {currentHeight}</h1>
+//     </div>
+//   );
+// }
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+    </div>
   )
 }
